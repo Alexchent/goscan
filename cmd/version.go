08a8ad3a64f60ae2ef0ab50b1ca17f414aab384c
Alexchent <1023615202@qq.com>
2023-09-19 20:03:33 +0800

    打印更多信息

--- cmd/version.go
+++ cmd/version.go
@@ -15,15 +15,15 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number",
 	Long:  `All software has versions. This is goscan`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v1.0.0")
+		fmt.Println("当前版本是：v1.0.0")
 
 		//fmt.Println(viper.AllKeys())
 		//fmt.Println("userLicense:", userLicense)
 		//fmt.Println("author:", viper.Get("author"))
 		//fmt.Println("useviper:", viper.Get("useviper"))
 		//fmt.Println("license:", viper.Get("license"))
-		fmt.Println(mconf.Conf.Dir)
-		fmt.Println(mconf.Conf.FilterType)
-		fmt.Println(mconf.Conf.Cache)
+		fmt.Println("导出文件保存位置：", mconf.Conf.Dir)
+		fmt.Println("扫描时忽略掉的文件类型：", mconf.Conf.FilterType)
+		fmt.Println("redis配置：", mconf.Conf.Cache)
 	},
 }
